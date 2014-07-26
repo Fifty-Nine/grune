e45c2a2d040e4b634e286e9c636c201d541b31a7
Fifty-Nine <tim.prince@gmail.com>
2014-07-25 22:31:50 -0700

    Add tests for production

---
+++ tests/production_tests.cpp
@@ -0,0 +1,25 @@
+#include "tests/tests.hpp"
+#include "grammar/production.hpp"
+
+using namespace grammar;
+
+class production_test : public CppUnit::TestFixture 
+{
+    CPPUNIT_TEST_SUITE(production_test);
+    CPPUNIT_TEST(testInitialized);
+    CPPUNIT_TEST_SUITE_END();
+
+public:
+    void testInitialized()
+    {
+        non_terminal nt;
+        production uninit;
+        production init(&nt);
+
+        CPPUNIT_ASSERT(!uninit.initialized());
+        CPPUNIT_ASSERT(init.initialized());
+    }
+    
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(production_test);
