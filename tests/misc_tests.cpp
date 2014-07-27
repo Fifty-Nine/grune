a8f3bea939f29e92e359b862948950ce53ac5eac
Fifty-Nine <tim.prince@gmail.com>
2014-07-26 18:14:39 -0700

    Test is_empty

--- tests/misc_tests.cpp
+++ tests/misc_tests.cpp
@@ -7,18 +7,21 @@ class misc_tests : public CppUnit::TestFixture
 {
     CPPUNIT_TEST_SUITE(misc_tests);
     CPPUNIT_TEST(test_is_terminal);
+    CPPUNIT_TEST(test_is_empty);
     CPPUNIT_TEST_SUITE_END();
 
 public:
     void test_is_terminal()
     {
         literal lit("l");
+        literal empty;
         non_terminal nt("nt");
         sequence nt_seq { &lit, &nt };
         sequence t_seq { &lit, &lit };
         sentence s { &lit, &lit };
 
         CPPUNIT_ASSERT(is_terminal(lit));
+        CPPUNIT_ASSERT(is_terminal(empty));
         CPPUNIT_ASSERT(!is_terminal(nt)); 
         CPPUNIT_ASSERT(!is_terminal(nt_seq));
         CPPUNIT_ASSERT(is_terminal(t_seq));
@@ -31,6 +34,16 @@ public:
         CPPUNIT_ASSERT(is_terminal(tp));
     }
     
+    void test_is_empty()
+    {
+        literal lit("l");
+        literal empty;
+        non_terminal nt("nt");
+
+        CPPUNIT_ASSERT(!is_empty(lit));
+        CPPUNIT_ASSERT(is_empty(empty));
+        CPPUNIT_ASSERT(!is_empty(nt));
+    }
 };
 
 CPPUNIT_TEST_SUITE_REGISTRATION(misc_tests);
