5527767beba2294519117788be9a375021a4bb64
Fifty-Nine <tim.prince@gmail.com>
2014-08-01 19:48:21 -0700

    Add YAML emit/parse test skeleton.

---
+++ unit_tests/yaml_tests.cpp
@@ -0,0 +1,73 @@
+#include "unit_tests/tests.hpp"
+#include "grune-yaml/grune-yaml.hpp"
+
+#include "grune/non_terminal.hpp"
+#include "grune/symbol.hpp"
+
+#include <sstream>
+
+using namespace grune;
+
+class yaml_tests : public CppUnit::TestFixture
+{
+    CPPUNIT_TEST_SUITE(yaml_tests);
+    CPPUNIT_TEST(test_symbol);
+    CPPUNIT_TEST(test_sequence); 
+    CPPUNIT_TEST(test_sequence_list);
+    CPPUNIT_TEST(test_production);
+    CPPUNIT_TEST(test_grammar);
+    CPPUNIT_TEST_SUITE_END();
+
+    template<class T>
+    bool equal_end_to_end(T value)
+    {
+        T first_pass = 
+            YAML::Load(YAML::Dump(YAML::Node(value))).as<T>();
+        T second_pass = 
+            YAML::Load(YAML::Dump(YAML::Node(first_pass))).as<T>();
+        return first_pass == value && second_pass == value;
+    }
+
+    template<class T>
+    bool load_equal(const std::string& yaml_doc, T value)
+    {
+        YAML::Node node = YAML::Load(yaml_doc);
+        CPPUNIT_ASSERT(YAML::Load(yaml_doc).as<T>() == value);
+    }
+
+public:
+    void test_symbol()
+    {
+        symbol empty;
+        symbol asdf("asdf");
+        non_terminal A("A");
+
+        CPPUNIT_ASSERT(equal_end_to_end(empty));
+        CPPUNIT_ASSERT(load_equal("{ is_terminal: yes, text: }", empty));
+
+        CPPUNIT_ASSERT(equal_end_to_end(asdf));
+        CPPUNIT_ASSERT(load_equal("{ is_terminal: yes, text: asdf }", asdf));
+
+        CPPUNIT_ASSERT(equal_end_to_end(A)); 
+        CPPUNIT_ASSERT(load_equal("{ is_terminal: no, text: A }", A));
+    }
+
+    void test_sequence()
+    {
+    }
+
+    void test_sequence_list()
+    {
+    }
+
+    void test_production()
+    {
+    }
+
+    void test_grammar()
+    {
+    }
+
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(yaml_tests);
