60a8c5893d37f14f80e9af09e73a2af7b5a1460d
Fifty-Nine <tim.prince@gmail.com>
2014-08-03 14:23:41 -0700

    Convert misc. tests to Boost UTF

--- unit_tests/misc_tests.cpp
+++ unit_tests/misc_tests.cpp
@@ -4,103 +4,95 @@
 
 using namespace grune;
 
-class misc_tests : public CppUnit::TestFixture 
+BOOST_AUTO_TEST_SUITE(misc_tests)
+
+BOOST_AUTO_TEST_CASE(is_terminal_test)
 {
-    CPPUNIT_TEST_SUITE(misc_tests);
-    CPPUNIT_TEST(test_is_terminal);
-    CPPUNIT_TEST(test_is_empty);
-    CPPUNIT_TEST(test_generate);
-    CPPUNIT_TEST_SUITE_END();
+    symbol lit("l");
+    symbol empty;
+    non_terminal nt("nt");
+    sequence nt_seq { lit, nt };
+    sequence t_seq { lit, lit };
+    sequence s { lit, lit };
 
-public:
-    void test_is_terminal()
+    BOOST_CHECK(is_terminal(lit));
+    BOOST_CHECK(is_terminal(empty));
+    BOOST_CHECK(!is_terminal(nt)); 
+    BOOST_CHECK(!is_terminal(nt_seq));
+    BOOST_CHECK(is_terminal(t_seq));
+    BOOST_CHECK(is_terminal(s));
+
+    production ntp0 { nt, { nt_seq } };
+    production ntp1 { nt, { t_seq } };
+    production_list ntp { ntp0, ntp1 };
+    production tp { nt, { t_seq } };
+
+    BOOST_CHECK(!is_terminal(ntp0));
+    BOOST_CHECK(is_terminal(ntp1));
+    BOOST_CHECK(!is_terminal(ntp));
+    BOOST_CHECK(is_terminal(tp));
+}
+
+BOOST_AUTO_TEST_CASE(is_empty_test)
+{
+    symbol lit("l");
+    symbol empty;
+    non_terminal nt("nt");
+
+    BOOST_CHECK(!is_empty(lit));
+    BOOST_CHECK(is_empty(empty));
+    BOOST_CHECK(!is_empty(nt));
+}
+
+BOOST_AUTO_TEST_CASE(generate_test)
+{
+    sequence_list result = generate(grammars::tom_dick_and_harry(), 5);
+
+    BOOST_CHECK_EQUAL(result.size(), (sequence::size_type)5);
+
+    sequence_list expected {
+        { "tom" },
+        { "dick" },
+        { "harry" }, 
+        { "tom", " and ", "tom" },
+        { "tom", " and ", "dick" }
+    };
+
+    BOOST_CHECK_EQUAL(expected, result);
+
+    non_terminal S("S");
+    non_terminal A("A");
+    non_terminal B("B");
+    grammar finite
     {
-        symbol lit("l");
-        symbol empty;
-        non_terminal nt("nt");
-        sequence nt_seq { lit, nt };
-        sequence t_seq { lit, lit };
-        sequence s { lit, lit };
-
-        CPPUNIT_ASSERT(is_terminal(lit));
-        CPPUNIT_ASSERT(is_terminal(empty));
-        CPPUNIT_ASSERT(!is_terminal(nt)); 
-        CPPUNIT_ASSERT(!is_terminal(nt_seq));
-        CPPUNIT_ASSERT(is_terminal(t_seq));
-        CPPUNIT_ASSERT(is_terminal(s));
-
-        production ntp0 { nt, { nt_seq } };
-        production ntp1 { nt, { t_seq } };
-        production_list ntp { ntp0, ntp1 };
-        production tp { nt, { t_seq } };
-
-        CPPUNIT_ASSERT(!is_terminal(ntp0));
-        CPPUNIT_ASSERT(is_terminal(ntp1));
-        CPPUNIT_ASSERT(!is_terminal(ntp));
-        CPPUNIT_ASSERT(is_terminal(tp));
-    }
-    
-    void test_is_empty()
-    {
-        symbol lit("l");
-        symbol empty;
-        non_terminal nt("nt");
-
-        CPPUNIT_ASSERT(!is_empty(lit));
-        CPPUNIT_ASSERT(is_empty(empty));
-        CPPUNIT_ASSERT(!is_empty(nt));
-    }
-
-    void test_generate()
-    {
-        sequence_list result = generate(grammars::tom_dick_and_harry(), 5);
-
-        CPPUNIT_ASSERT_EQUAL(result.size(), (sequence::size_type)5);
-
-        sequence_list expected {
-            { "tom" },
-            { "dick" },
-            { "harry" }, 
-            { "tom", " and ", "tom" },
-            { "tom", " and ", "dick" }
-        };
-
-        CPPUNIT_ASSERT_EQUAL(expected, result);
-
-        non_terminal S("S");
-        non_terminal A("A");
-        non_terminal B("B");
-        grammar finite
+        { S, A, B },
+        { 0, 1 },
         {
-            { S, A, B },
-            { 0, 1 },
-            {
-                /* S -> AB */
-                { S, { A, B } },
-                /* A -> 01 | 10 */
-                { A, { 0, 1 } },
-                { A, { 1, 0 } },
-                /* B -> 11 | 00 */
-                { B, { 1, 1 } },
-                { B, { 0, 0 } }
-            },
-            S
-        };
+            /* S -> AB */
+            { S, { A, B } },
+            /* A -> 01 | 10 */
+            { A, { 0, 1 } },
+            { A, { 1, 0 } },
+            /* B -> 11 | 00 */
+            { B, { 1, 1 } },
+            { B, { 0, 0 } }
+        },
+        S
+    };
 
-        sequence_list expected_finite 
-        {
-            { 0, 1, 1, 1 },
-            { 0, 1, 0, 0 },
-            { 1, 0, 1, 1 },
-            { 1, 0, 0, 0 },
-            { 0, 1, 1, 1 },
-            { 1, 0, 1, 1 },
-            { 0, 1, 0, 0 },
-            { 1, 0, 0, 0 }
-        };
+    sequence_list expected_finite 
+    {
+        { 0, 1, 1, 1 },
+        { 0, 1, 0, 0 },
+        { 1, 0, 1, 1 },
+        { 1, 0, 0, 0 },
+        { 0, 1, 1, 1 },
+        { 1, 0, 1, 1 },
+        { 0, 1, 0, 0 },
+        { 1, 0, 0, 0 }
+    };
 
-        CPPUNIT_ASSERT_EQUAL(expected_finite, generate(finite));
-    }
-};
+    BOOST_CHECK_EQUAL(expected_finite, generate(finite));
+}
 
-CPPUNIT_TEST_SUITE_REGISTRATION(misc_tests);
+BOOST_AUTO_TEST_SUITE_END()
