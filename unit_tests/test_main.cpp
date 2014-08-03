1d8a28960a5ffa45afcb5edaa13b57a26b7f9bb8
Fifty-Nine <tim.prince@gmail.com>
2014-08-03 14:43:01 -0700

    Remove remaining references to CppUnit

--- unit_tests/test_main.cpp
+++ unit_tests/test_main.cpp
@@ -1,25 +1,3 @@
-#include "grune/all.hpp"
-
 #define BOOST_TEST_MAIN
 #define BOOST_TEST_DYN_LINK
-#include "unit_tests/tests.hpp"
-
-using namespace CppUnit;
-
-CompilerOutputter *getOutputter(TestResultCollector& collector)
-{
-    CompilerOutputter *result = 
-        new CompilerOutputter(&collector, std::cerr);
-
-    result->setLocationFormat("%p:%l:");
-
-    return result;
-}
-
-BOOST_AUTO_TEST_CASE(cppunit_main)
-{
-    TextTestRunner runner;
-    runner.addTest(TestFactoryRegistry::getRegistry().makeTest());
-    runner.setOutputter(getOutputter(runner.result()));
-    BOOST_CHECK(runner.run("", false, true, false));
-}
+#include <boost/test/unit_test.hpp>
--- unit_tests/tests.hpp
+++ unit_tests/tests.hpp
@@ -1,49 +1,25 @@
 #ifndef TESTS_H
 #define TESTS_H
 
-#include <boost/test/unit_test.hpp>
-#include <cppunit/CompilerOutputter.h>
-#include <cppunit/extensions/HelperMacros.h>
-#include <cppunit/TestAssert.h>
-#include <cppunit/TestFixture.h>
-#include <cppunit/TestSuite.h>
-#include <cppunit/TextTestRunner.h>
-
 #include "grune/grune_fwd.hpp"
 #include "grune/sentence_iterator.hpp"
 #include "grune-yaml/grune-yaml.hpp"
 
-template<class T>
-struct grune_assertion_traits
-{
-    static bool equal(const T& x, const T& y)
-    {
-        return x == y;
-    }
+#include <boost/test/unit_test.hpp>
 
-    static std::string toString(const T& x)
-    {
-#ifndef GRUNE_ASSERTION_TRAITS_NO_YAML
-        return YAML::Dump(YAML::Node(x));
-#else /* GRUNE_ASSERTION_TRAITS_NO_YAML */
-        return grune::to_string(x);
-#endif
-    }
-
-};
+#ifdef GRUNE_ASSERTION_TRAITS_NO_YAML
+#define GRUNE_TO_STRING(x) grune::to_string(x)
+#else
+#define GRUNE_TO_STRING(x) YAML::Dump(YAML::Node(x));
+#endif /* GRUNE_ASSERTION_TRAITS_NO_YAML */
 
 #define GRUNE_ASSERTION_TRAITS(t) \
-    namespace CppUnit { \
-    template<> \
-    struct assertion_traits<t> : \
-        public grune_assertion_traits<t> \
-    { }; }\
     namespace boost { namespace test_tools { \
     template<> \
     struct print_log_value<t> { \
         template<class T=void> \
         void operator()(std::ostream& os, const t& v) { \
-            grune_assertion_traits<t>::toString(v); \
+            os << GRUNE_TO_STRING(v); \
         } \
     }; \
     } }
@@ -56,25 +32,6 @@ GRUNE_ASSERTION_TRAITS(grune::production);
 GRUNE_ASSERTION_TRAITS(grune::production_list);
 GRUNE_ASSERTION_TRAITS(grune::grammar);
 
-namespace CppUnit {
-template<>
-struct assertion_traits<grune::sentence_iterator>
-{
-    typedef grune::sentence_iterator T;
-    static bool equal(const T& x, const T& y) { return x == y; }
-    static std::string toString(const T& x)
-    {
-        using namespace grune;
-        if (x != sentence_iterator())
-        {
-            return assertion_traits<sequence>::toString(*x);
-        }
-        return "<end>";
-    }
-};
-
-}
-
 namespace boost { 
 namespace test_tools { 
     template<>
@@ -82,7 +39,10 @@ namespace test_tools {
     {
         void operator()(std::ostream& os, const grune::sentence_iterator& v) 
         {
-            os << CppUnit::assertion_traits<grune::sentence_iterator>::toString(v);
+            auto r = 
+                (v == grune::sentence_iterator()) ? 
+                "<end>" : grune::to_string(*v);
+            os << r;
         }
     };
 } 
