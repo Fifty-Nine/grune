f3371f8e37149e4fe87f48bb0b608f31dbacebf2
Fifty-Nine <tim.prince@gmail.com>
2014-08-03 14:26:56 -0700

    Convert production tests to Boost UTF

--- unit_tests/production_tests.cpp
+++ unit_tests/production_tests.cpp
@@ -3,92 +3,83 @@
 
 using namespace grune;
 
-class production_test : public CppUnit::TestFixture 
+BOOST_AUTO_TEST_SUITE(production_test)
+
+BOOST_AUTO_TEST_CASE(initialized_test)
 {
-    CPPUNIT_TEST_SUITE(production_test);
-    CPPUNIT_TEST(test_initialized);
-    CPPUNIT_TEST(test_to_string);
-    CPPUNIT_TEST(test_apply);
-    CPPUNIT_TEST_SUITE_END();
+    non_terminal nt("S");
+    production uninit;
+    production init { nt };
 
-public:
-    void test_initialized()
+    BOOST_CHECK(!uninit.initialized());
+    BOOST_CHECK(init.initialized());
+}
+
+BOOST_AUTO_TEST_CASE(to_string_test)
+{
+    non_terminal left("left");
+    non_terminal right("right");
+    symbol l1("1");
+    symbol l2("2");
+
+    production p { left, { right, l1, l2 } };
+
+    std::string expected = "left = right, \"1\", \"2\"";
+    BOOST_CHECK_EQUAL(expected, p.to_string());
+
+    production p2_1 { right, { l1, l2 } };
+    production p2_2 { right, { left, left } };
+    production_list p2 { p2_1, p2_2 };
+
+    expected = "right = \"1\", \"2\"";
+    BOOST_CHECK_EQUAL(expected, to_string(p2_1));
+
+    expected = "right = left, left";
+    BOOST_CHECK_EQUAL(expected, to_string(p2_2));
+
+    expected = "right = \"1\", \"2\" | left, left;\n";
+    BOOST_CHECK_EQUAL(expected, to_string(p2));
+
+    production p3_1 { { left, right }, { l1, right } };
+    production p3_2 { { left, right }, { left, l2 } };
+    production_list p3 { p3_1, p3_2 };
+
+    expected = "left, right = \"1\", right";
+    BOOST_CHECK_EQUAL(expected, to_string(p3_1));
+
+    expected = "left, right = left, \"2\"";
+    BOOST_CHECK_EQUAL(expected, to_string(p3_2));
+
+    expected = "left, right = \"1\", right | left, \"2\";\n";
+    BOOST_CHECK_EQUAL(expected, to_string(p3));
+
+    production empty_rhs(left);
+    expected = "left = \"\"";
+    BOOST_CHECK_EQUAL(expected, to_string(empty_rhs));
+}
+
+BOOST_AUTO_TEST_CASE(apply_test)
+{
+    non_terminal A("A");
+    non_terminal B("B");
+    non_terminal C("C");
+
+    production p1_1 { A, { "b", "c" } };
+    production p1_2 { A, { "d" } };
+    production_list p1 { p1_1, p1_2 };
+
+    BOOST_CHECK(apply(p1_1, {}).empty());
+    BOOST_CHECK(apply(p1_2, {}).empty());
+    BOOST_CHECK(apply(p1, {}).empty());
+
+    sequence_list result = apply(p1, {"x", A, "y"});
+    BOOST_CHECK_EQUAL((int)result.size(), 2);
+
+    sequence_list expected = 
     {
-        non_terminal nt("S");
-        production uninit;
-        production init { nt };
-
-        CPPUNIT_ASSERT(!uninit.initialized());
-        CPPUNIT_ASSERT(init.initialized());
-    }
-    
-    void test_to_string()
-    {
-        non_terminal left("left");
-        non_terminal right("right");
-        symbol l1("1");
-        symbol l2("2");
-
-        production p { left, { right, l1, l2 } };
-
-        std::string expected = "left = right, \"1\", \"2\"";
-        CPPUNIT_ASSERT_EQUAL(expected, p.to_string());
-
-        production p2_1 { right, { l1, l2 } };
-        production p2_2 { right, { left, left } };
-        production_list p2 { p2_1, p2_2 };
-
-        expected = "right = \"1\", \"2\"";
-        CPPUNIT_ASSERT_EQUAL(expected, to_string(p2_1));
-
-        expected = "right = left, left";
-        CPPUNIT_ASSERT_EQUAL(expected, to_string(p2_2));
-
-        expected = "right = \"1\", \"2\" | left, left;\n";
-        CPPUNIT_ASSERT_EQUAL(expected, to_string(p2));
-
-        production p3_1 { { left, right }, { l1, right } };
-        production p3_2 { { left, right }, { left, l2 } };
-        production_list p3 { p3_1, p3_2 };
-
-        expected = "left, right = \"1\", right";
-        CPPUNIT_ASSERT_EQUAL(expected, to_string(p3_1));
-
-        expected = "left, right = left, \"2\"";
-        CPPUNIT_ASSERT_EQUAL(expected, to_string(p3_2));
-
-        expected = "left, right = \"1\", right | left, \"2\";\n";
-        CPPUNIT_ASSERT_EQUAL(expected, to_string(p3));
-
-        production empty_rhs(left);
-        expected = "left = \"\"";
-        CPPUNIT_ASSERT_EQUAL(expected, to_string(empty_rhs));
-    }
-
-    void test_apply()
-    {
-        non_terminal A("A");
-        non_terminal B("B");
-        non_terminal C("C");
-
-        production p1_1 { A, { "b", "c" } };
-        production p1_2 { A, { "d" } };
-        production_list p1 { p1_1, p1_2 };
-
-        CPPUNIT_ASSERT(apply(p1_1, {}).empty());
-        CPPUNIT_ASSERT(apply(p1_2, {}).empty());
-        CPPUNIT_ASSERT(apply(p1, {}).empty());
-
-        sequence_list result = apply(p1, {"x", A, "y"});
-        CPPUNIT_ASSERT_EQUAL((int)result.size(), 2);
-
-        sequence_list expected = 
-        {
-            { "x", "b", "c", "y" },
-            { "x", "d", "y" }
-        };
-        CPPUNIT_ASSERT_EQUAL(result, expected);
-    }
-};
-
-CPPUNIT_TEST_SUITE_REGISTRATION(production_test);
+        { "x", "b", "c", "y" },
+        { "x", "d", "y" }
+    };
+    BOOST_CHECK_EQUAL(result, expected);
+}
+BOOST_AUTO_TEST_SUITE_END()
