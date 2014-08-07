462fc586c99519c576c36f85ab68a212f835d94e
Fifty-Nine <tim.prince@gmail.com>
2014-08-06 20:48:25 -0700

    Add production serialization.

--- grune-json/production.cpp
+++ grune-json/production.cpp
@@ -3,14 +3,23 @@
 #include "grune/production.hpp"
 
 using namespace grune;
+using namespace json11;
 
-json11::Json grune::to_json(const production& value)
+Json grune::to_json(const production& value)
 {
-    return { };
+    Json lhs(value.lhs());
+    Json rhs(value.rhs());
+    return Json::object { 
+        { "lhs", lhs },
+        { "rhs", rhs }
+    };
 }
 
-bool grune::from_json(const json11::Json& js, production& p)
+bool grune::from_json(const Json& js, production& p)
 {
-    return false;
+    sequence lhs = js["lhs"].as<sequence>();
+    sequence rhs = js["rhs"].as<sequence>();
+    p = production(lhs, rhs);
+    return true;
 }
 
